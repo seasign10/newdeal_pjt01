9d4240436cfad96609d388d762054cd48973f197
seasign10 <seasign10@gmail.com>
2024-05-21 15:09:25 +0900

    1st commit

--- script/status.js
+++ script/status.js
@@ -1,3 +1,6 @@
+// survice_key
+const API_KEY = config.apikey;
+
 // 시간데이터를 사용할 일이 있으면 사용하자.
 // let getNow = new Date();
 // let year = getNow.getFullYear();
@@ -9,7 +12,7 @@ const getData = async ()=>{
 
   // 서울 코드
   const localSeoul = '%EC%84%9C%EC%9A%B8';
-  const url = new URL(`https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=KWnKkw4c%2FCEAvAiJWCNfpPBpO9g0nW2u7KSwDkaM0wcG930KuF%2F9vy32a5Xc5fyPnP0sSOD2IVFZ0vZkrBcmmw%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName=${localSeoul}&ver=1.0`);
+  const url = new URL(`https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${API_KEY}&returnType=json&numOfRows=100&pageNo=1&sidoName=${localSeoul}&ver=1.0`);
   const response = await fetch(url);
   const data = await response.json();
   // console.log(data.response.body.items);
@@ -22,17 +25,31 @@ getData();
 
 // 서울
 const add = '중구';
+
+let dustGrade = '';
+let dustValue = '';
 let dustContent = ``
 // let statusNow = ()=>{};
 // function statusNow(){};
-function statusNowUI(data) {
+function statusNowUI(data){
   // console.log(data.length);
   // grade = 좋음~나쁨 | value : 측정값
   for(let i=0;i<data.length;i++){
     if(data[i].stationName == add){
-      console.log(data[i].so2Grade);
+      dustGrade = data[i].khaiGrade;
+      dustValue = data[i].khaiValue;
     }
   }
-
+  console.log(dustGrade);
+  console.log(dustValue);
+  dustFilter(dustGrade, dustValue);
   // document.querySelector('.dust_situation').innerHTML = dustContent;
+}
+
+function dustFilter(g, v){
+  // 좋음~나쁨
+  // typeof로 찍어보니 string값
+  if(g=='1'){
+    
+  }
 }

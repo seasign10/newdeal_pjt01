7c6e6fd0217d6d86a9315254cad30f280da64c30
HaeIn Lee <seasign10@gmail.com>
2025-02-26 22:15:46 +0900

    add: netlify

---
+++ netlify/functions/getEnv.js
@@ -0,0 +1,11 @@
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      API_KEY: process.env.API_KEY, // Netlify 환경변수에서 가져옴
+      VWORLD_API_KEY: process.env.VWORLD_API_KEY,
+      NAVER_API_CLIENT_ID: process.env.NAVER_API_CLIENT_ID,
+      NAVER_API_KEY: process.env.NAVER_API_KEY,
+    }),
+  };
+};
